f882fb9d113cba3c976dd121744237c4abc21268
Simppa <simo.siren@careeria.fi>
2025-08-22 12:36:45 +0300

    silmukka

---
+++ silmukka.js
@@ -0,0 +1,12 @@
+// Alustetaan taulukko
+const puhelimet = ["Apple", "Samsung", "Honor"]
+
+// Käydään em. taulukko läpi forEach silmukassa
+// jokainen alkio saa aliasnimen puh
+
+puhelimet.forEach(puh => {
+    console.log(puh)
+})
+
+// Suorita: node silmukat.js
+
