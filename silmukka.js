53a826688dcd9f22f6ac56ba6da3f345416225d8
Simppa <simo.siren@careeria.fi>
2025-08-22 12:48:33 +0300

    lisää

--- silmukka.js
+++ silmukka.js
@@ -1,11 +1,19 @@
 // Alustetaan taulukko
-const puhelimet = ["Apple", "Samsung", "Honor"]
+const puhelimet = ["Oppo", "Apple", "Samsung", "Honor"]
 
 // Käydään em. taulukko läpi forEach silmukassa
 // jokainen alkio saa aliasnimen puh
 
 puhelimet.forEach(puh => {
+
+    if (puh === "Apple") {
+         console.log("Tämä EI OLE Android puhelin: ")
+    }
+    else {
+        console.log("Tämä ON Android puhelin: ")
+    }
     console.log(puh)
+    console.log("-------------------------")
 })
 
 // Suorita: node silmukka.js
